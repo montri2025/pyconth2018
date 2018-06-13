337d254dbf4fd3963e47e6a239a2238585866113
Montri Soymukda <montri.s@olx.co.th>
2018-06-13 09:24:27 +0700

    exception

--- worker/worker.py
+++ worker/worker.py
@@ -21,35 +21,38 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 #@statsd.timer(worker_name + '.on-message')
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    msg_json = json.loads(msg.payload)
-    if isinstance(msg_json,dict):
-        # d = deposite
-        if msg_json.get('deposite'):
-            amount_deposite = msg_json.get('deposite',0)
-            deposite(PIGGY_ID,amount_deposite,'desc deposite')
-            statsd.incr('deposite',amount_deposite)
-            statsd.gauge('deposite',amount_deposite)
-            current_balance = balance(PIGGY_ID)
-            statsd.gauge('balance',current_balance)
-            print("deposite::", amount_deposite)
-            print("balance::",current_balance)
+    try:
+        print(msg.topic+" "+str(msg.payload))
+        msg_json = json.loads(msg.payload)
+        if isinstance(msg_json,dict):
+            # d = deposite
+            if msg_json.get('deposite'):
+                amount_deposite = msg_json.get('deposite',0)
+                deposite(PIGGY_ID,amount_deposite,'desc deposite')
+                statsd.incr('deposite',amount_deposite)
+                statsd.gauge('deposite',amount_deposite)
+                current_balance = balance(PIGGY_ID)
+                statsd.gauge('balance',current_balance)
+                print("deposite::", amount_deposite)
+                print("balance::",current_balance)
 
-        elif msg_json.get('withdraw'):
-            amount_withdraw = withdraw(PIGGY_ID,'desc withdraw')
-            statsd.gauge('withdraw',amount_withdraw)
-            statsd.incr('withdraw',amount_withdraw)
-            current_balance = balance(PIGGY_ID)
-            statsd.gauge('balance',current_balance)
-            print("withdraw::", amount_withdraw)
-            print("balance::",current_balance)
-        elif msg_json.get('online'):
-             statsd.gauge('online',msg_json.get('online'))  
-        else:
-            current_balance = balance(PIGGY_ID)
-            statsd.gauge('balance',current_balance)
-            statsd.incr('balance',current_balance)
-            print("balance::",current_balance)
+            elif msg_json.get('withdraw'):
+                amount_withdraw = withdraw(PIGGY_ID,'desc withdraw')
+                statsd.gauge('withdraw',amount_withdraw)
+                statsd.incr('withdraw',amount_withdraw)
+                current_balance = balance(PIGGY_ID)
+                statsd.gauge('balance',current_balance)
+                print("withdraw::", amount_withdraw)
+                print("balance::",current_balance)
+            elif msg_json.get('online'):
+                statsd.gauge('online',msg_json.get('online'))  
+            else:
+                current_balance = balance(PIGGY_ID)
+                statsd.gauge('balance',current_balance)
+                statsd.incr('balance',current_balance)
+                print("balance::",current_balance)
+    except Exception as e:
+        print(e)          
             
 print("MQTT_ID::",MQTT_CLIENT_ID,"MQTT_SERVER::",MQTT_SERVER)        
 client = mqtt.Client(MQTT_CLIENT_ID)
