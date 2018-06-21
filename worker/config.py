40b58c5136d1818853341a870cf3687d338d96d5
montri2025 <montri2025@gmail.com>
2018-06-21 13:04:18 +0700

    localhost
    localhost

--- worker/config.py
+++ worker/config.py
@@ -1,7 +1,7 @@
 
-MQTT_SERVER = "deenaja.com"
+MQTT_SERVER = "localhost"
 PIGGY_ID = 1
 SUBSCRIBE = "pyconth/2018/piggy/bank/"+ str(PIGGY_ID)
-STATSD_HOST = "deenaja.com"
+STATSD_HOST = "localhost"
 STATSD_PORT = "8125"
 STATSD_PREFIX = "pigg.id."+ str(PIGGY_ID)
