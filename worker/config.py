b69f3ba3d2ba1dae0cd913eefc5dab6743a7ab1f
montri2025 <montri2025@gmail.com>
2018-07-05 22:27:17 +0700

    Update config.py

--- worker/config.py
+++ worker/config.py
@@ -1,7 +1,7 @@
 
-MQTT_SERVER = "172.17.0.1"
+MQTT_SERVER = "deenaja.com"
 PIGGY_ID = 1
 SUBSCRIBE = "pyconth/2018/piggy/bank/"+ str(PIGGY_ID)
-STATSD_HOST = "172.17.0.1"
+STATSD_HOST = "deenaja.com"
 STATSD_PORT = "8125"
 STATSD_PREFIX = "pigg.id."+ str(PIGGY_ID)
